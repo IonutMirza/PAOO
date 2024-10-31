a6d1bf8d13c0ea33d4ff4edfcbc870404f199d2b
IonutMirza <91843904+IonutMirza@users.noreply.github.com>
2024-10-31 19:15:26 +0200

    Add CMake

---
+++ Tema1 PAOO/build/CMakeFiles/CarProject.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CarProject.dir/Car.cpp.obj"
+  "CMakeFiles/CarProject.dir/Car.cpp.obj.d"
+  "CMakeFiles/CarProject.dir/main.cpp.obj"
+  "CMakeFiles/CarProject.dir/main.cpp.obj.d"
+  "CarProject.exe"
+  "CarProject.exe.manifest"
+  "CarProject.pdb"
+  "libCarProject.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CarProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Tema1 PAOO/build/CMakeFiles/CarProject.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for CarProject.
+# This may be replaced when dependencies are built.
